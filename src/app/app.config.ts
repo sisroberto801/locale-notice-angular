0a4db9263f3fa519916bc881aa1fb46d68052850
rolguin <sis.roberto801@gmail.com>
2026-08-09 07:13:40 -0400

    add providers and config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,9 @@
-import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
-} from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideHttpClient(withFetch()),
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-  ],
+  providers: [provideHttpClient(), provideBrowserGlobalErrorListeners(), provideRouter(routes)],
 };
